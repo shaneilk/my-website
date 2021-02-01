e64a4dfd367decbfacbba10b316039b714aee224
Shaneil <you@example.com>
2021-02-01 14:04:10 -0800

    deploy my react app to github today

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import profile from './img/shaneilProfile.jpeg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My react App</h1>
+      <h2>Author: Author Shaneil Kapadia</h2>
+      <img src={profile} alt="profile-image" />
     </div>
   );
 }
