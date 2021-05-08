0e354315d2130d056032b3b6393ff4cd55c1b002
shaneilk <shaneil1024@gmail.com>
2021-05-08 13:06:15 -0700

    added BrowserRouter to path

--- src/App.js
+++ src/App.js
@@ -21,18 +21,19 @@ import './App.css';
 import Home from './Home.js'
 import AboutMe from './AboutMe.js'
 import Projects from './Projects.js'
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar.js';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Switch>
-        <Route exact path="/my-website" component={Projects} />
-        <Route path="/my-website/about" component={AboutMe} />
-      </Switch>
-      
+      <Router>
+        <Switch>
+          <Route exact path="/my-website" component={Projects} />
+          <Route path="/my-website/about" component={AboutMe} />
+        </Switch>
+      </Router>
     </div>
   );
 }
